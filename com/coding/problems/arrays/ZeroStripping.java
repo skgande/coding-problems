6a35990e401a51eee893ed4b643aa6fba497d292
Sunilkumar Gande <gande.sunilkumar@gmail.com>
2025-01-22 00:18:47 -0600

    Zerostripping problem

--- com/coding/problems/arrays/ZeroStripping.java
+++ com/coding/problems/arrays/ZeroStripping.java
@@ -1,8 +1,5 @@
 package com.coding.problems.arrays;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class ZeroStripping {
     public static void main(String[] args) {
         int[][] a  = {
@@ -11,19 +8,8 @@ public class ZeroStripping {
                 {11, 12, 13, 14, 15},
                 {16, 17, 18, 19, 0}
         };
-        int m = a.length;
-        int n = a[0].length;
-        Set<int[]> set = new HashSet<int[]>();
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if(a[i][j] == 0) {
-                    set.add(new int[] {i, j});
-                }
-            }
-        }
-
-        setZeros(a, set, m, n);
+        int m = a.length, n = a[0].length;
+        setZeros(a, m, n);
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
@@ -33,14 +19,23 @@ public class ZeroStripping {
         }
     }
 
-    private static void setZeros(int[][] a, Set<int[]> set, int m, int n) {
-        for(int[] k: set) { //{3, 4}
-            for(int i = 0; i < n; i++) {
-                a[k[0]][i] = 0;
-            }
+    private static void setZeros(int[][] a, int m, int n) {
+        boolean[] row = new boolean[m];
+        boolean[] col = new boolean[n];
 
-            for(int i = 0; i < m; i++) {
-                a[i][k[1]] = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if(a[i][j] == 0) {
+                    row[i] = col[j] = true;
+                }
+            }
+        }
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(row[i] | col[j]) {
+                    a[i][j] = 0;
+                }
             }
         }
     }
